b761995bfddc2276ca79cbc555f7cdf82df18fab
Jullia Akutagawa <61246467+JulliaAtsumi@users.noreply.github.com>
2023-07-15 00:11:31 -0300

    Create ex02.dart

---
+++ ex02.dart
@@ -0,0 +1,5 @@
+/*
+Faça	 uma	 programa	 para	 representar	a	árvore	genealógica	 de	 uma	 família.	 Para	 tal,	 crie	
+uma	classe	Pessoa	que	permita	indicar,	além	de	nome	e	idade,	o	pai	e	a	mãe.	Tenha	em	
+mente	que	pai	e	mãe	também	são	do	tipo	Pessoa.
+*/
